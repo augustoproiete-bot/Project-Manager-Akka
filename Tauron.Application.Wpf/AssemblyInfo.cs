cabacd1e530ea7e717ab5af86460875087473687
Alexander Bachmaier <Tauron.ab@gmail.com>
2020-06-18 06:16:36 +0200

    Commit

--- Tauron.Application.Wpf/AssemblyInfo.cs
+++ Tauron.Application.Wpf/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("http://schemas.TauronWpf.com", "Tauron.Application.Wpf.Converter")]
 [assembly: XmlnsDefinition("http://schemas.TauronWpf.com", "Tauron.Application.Wpf.Model")]
 [assembly: XmlnsDefinition("http://schemas.TauronWpf.com", "Tauron.Application.Wpf.UI")]
+[assembly: XmlnsDefinition("http://schemas.TauronWpf.com", "Tauron.Application.Wpf.Dialogs")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
