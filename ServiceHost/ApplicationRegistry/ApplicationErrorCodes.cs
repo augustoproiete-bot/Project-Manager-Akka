847be03e30bd6928e402739b48bda7d959f025c7
Alexander Bachmaier <Tauron.ab@gmail.com>
2020-07-13 12:13:12 +0200

    Commit

--- ServiceHost/ApplicationRegistry/ApplicationErrorCodes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServiceHost.ApplicationRegistry
-{
-    public static class ApplicationErrorCodes
-    {
-        public const string InvalidPath = nameof(InvalidPath);
-
-        public const string InvalidVersion = nameof(InvalidVersion);
-    }
-}
