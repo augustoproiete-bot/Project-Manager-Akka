5dab7582770711d6a90b3a33c07ad1050553ae17
Alexander Bachmaier <Tauron.ab@gmail.com>
2020-07-24 06:18:59 +0200

    Commit

--- ServiceHost/Program.cs
+++ ServiceHost/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Cluster;
 using Autofac;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.CommandLine;
+using Newtonsoft.Json;
 using Serilog;
 using ServiceHost.Installer;
 using ServiceHost.Services;
@@ -44,8 +50,19 @@ namespace ServiceHost
                 }
                 else
                 {
-                    var config = new ConfigurationBuilder().AddCommandLine(args).Build();
-
+                    try
+                    {
+                        await using var client = new NamedPipeClientStream(".", MonitorName, PipeDirection.In, PipeOptions.Asynchronous);
+                        await client.ConnectAsync(10000, CancellationToken.None);
+                        
+                        byte[] data = Encoding.UTF8.GetBytes(new ExposedCommandLineProvider(args).Serialize());
+                        await client.WriteAsync(BitConverter.GetBytes(data.Length));
+                        await client.WriteAsync(data);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
                 }
             }
             finally
@@ -53,6 +70,17 @@ namespace ServiceHost
                 if (createdNew)
                     m.ReleaseMutex();
                 IncomingCommandHandler.Handler?.Stop();
+            }
+        }
+
+        private sealed class ExposedCommandLineProvider : CommandLineConfigurationProvider
+        {
+            public ExposedCommandLineProvider(IEnumerable<string> args) : base(args) { }
+
+            public string Serialize()
+            {
+                Load();
+                return JsonConvert.SerializeObject(Data);
             }
         }
 
@@ -70,32 +98,67 @@ namespace ServiceHost
         public sealed class IncomingCommandHandler
         {
             private readonly Func<IConfiguration, ManualInstallationTrigger> _installTrigger;
-            private readonly NamedPipeServerStream _reader = new NamedPipeServerStream(MonitorName, PipeDirection.In, 1);
+            private readonly NamedPipeServerStream _reader = new NamedPipeServerStream(MonitorName, PipeDirection.Out, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
             private readonly CancellationTokenSource _cancellationToken = new CancellationTokenSource();
 
             public IncomingCommandHandler(Func<IConfiguration, ManualInstallationTrigger> installTrigger)
             {
                 _installTrigger = installTrigger;
-                Task.Factory.StartNew(Reader, TaskCreationOptions.LongRunning);
+                Task.Factory.StartNew(async () => await Reader(), TaskCreationOptions.LongRunning);
             }
 
-            private void Reader()
+            private async Task Reader()
             {
-                
+
                 try
                 {
-                    _reader.WaitForConnectionAsync(_cancellationToken.Token);
-                    BitConverter.GetBytes()
+                    while (true)
+                    {
+                        await _reader.WaitForConnectionAsync(_cancellationToken.Token);
+
+                        var buffer = MemoryPool<byte>.Shared.Rent(4);
+                        if (!await TryRead(buffer, 4, _cancellationToken.Token)) continue;
+
+                        var count = BitConverter.ToInt32(buffer.Memory.Span);
+                        buffer = MemoryPool<byte>.Shared.Rent(count);
+
+                        if (await TryRead(buffer, count, _cancellationToken.Token)) 
+                            ParseData(Encoding.UTF8.GetString(buffer.Memory.Slice(0, count).Span));
+                    }
                 }
-                catch(OperationCanceledException)
-                {}
+                catch (OperationCanceledException)
+                {
+                }
                 catch (Exception e)
                 {
                     Log.Logger.Error(e, "Error on Read CommandLine from outer process");
                 }
 
-                _reader.Dispose();
+                await _reader.DisposeAsync();
                 Handler = null;
+            }
+
+            private async Task<bool> TryRead(IMemoryOwner<byte> buffer, int lenght, CancellationToken token)
+            {
+                var currentLenght = 0;
+
+                while (true)
+                {
+                    if (_reader.IsMessageComplete)
+                        return currentLenght == lenght;
+                    if (currentLenght > lenght)
+                        return false;
+
+                    currentLenght = await _reader.ReadAsync(buffer.Memory.Slice(currentLenght, lenght - currentLenght), token);
+
+                    if (currentLenght == lenght)
+                        return _reader.IsMessageComplete;
+                }
+            }
+
+            private void ParseData(string data)
+            {
+
             }
 
             public void Stop() 
@@ -104,7 +167,7 @@ namespace ServiceHost
             public static void SetHandler(IncomingCommandHandler handler)
             {
                 if(Handler != null)
-                    handler.Dispose();
+                    handler.Stop();
                 else
                     Handler = handler;
             }
